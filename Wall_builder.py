91086e6efce6664de2ab84170e025bed333ccedc
Gopal Choudhary <gopalchoudhary493@gmail.com>
2023-07-18 22:00:26 +0530

    Create Wall_builder.py

---
+++ Wall_builder.py
@@ -0,0 +1,7 @@
+small_bricks = int(input('enter no. of small bricks'))
+Big_bricks = int(input('enter no. of big bricks'))
+goal = int(input('enter goal value'))
+if (small_bricks*1+Big_bricks*5>=goal):
+    print('true')
+else:
+     print('false')
