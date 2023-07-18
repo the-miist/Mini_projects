30ffc7de243cfb5779226154eb4a4ad311f2efed
Gopal Choudhary <gopalchoudhary493@gmail.com>
2023-07-18 22:26:24 +0530

    Create Calculator_using_functions.py

---
+++ Calculator_using_functions.py
@@ -0,0 +1,15 @@
+def addition(num_1,num_2):
+    res_add = num_1 + num_2
+    return res_add
+
+def subtraction(num_1,num_2):
+    res_sub = num_1 - num_2
+    return res_sub
+
+def muliplication(num_1,num_2):
+    res_mul = num_1 * num_2
+    return res_mul
+
+def division(num_1,num_2):
+    res_div = num_1 / num_2
+    return res_div
